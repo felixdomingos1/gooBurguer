d8ca6d691fb0192c9b41708356f793b0ff0103fc
felixdomingos1 <felixsdomingos93@gmail.com>
2025-06-11 08:33:01 +0100

    modify all way to pick any image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,40 @@
 import AuthInitializer from '@/components/AuthInitializer';
 import './globals.css';
+import { Metadata } from 'next';
 
 
+export const metadata: Metadata = {
+  title: "Gooburger - Hamburgers Deliciosos",
+  keywords: ["hamburgers", "entregas", "gooburger", "hamburgers deliciosos", "Peça um online"],
+  authors: [{ name: "Gooburger Team", url: "https://goo-burguer.vercel.app/" }],
+  creator: "Equipa Gooburger",
+  publisher: "Gooburger",
+  themeColor: "#ff5722",
+  colorScheme: "light dark",
+  description: "Explore nosso menu de hambúrgueres deliciosos no Gooburger. Encontre seu hambúrguer favorito e peça online para entrega ou retirada.",
+  openGraph: {
+    title: "Gooburger - Hamburgers Deliciosos",
+    description: "Explore nosso menu de hambúrgueres deliciosos no Gooburger. Encontre seu hambúrguer favorito e peça online para entrega ou retirada.",
+    images: [
+      {
+        url: "/images/7.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Gooburger - Delicious Hamburgers Deliciosos",
+      },
+    ],
+  },
+  twitter: {
+    title: "Gooburger - Hamburgers Deliciosos",
+    description: "Explore nosso menu de hambúrgueres deliciosos no Gooburger. Encontre seu hambúrguer favorito e peça online para entrega ou retirada.",
+    card: "summary_large_image",
+    images: ["/images/7.jpg"],
+  }, 
+  robots: {
+    index: true,
+    follow: true,
+  },
+};
 export default function RootLayout({
   children,
 }: {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 import BurgerListPage from "./BurgerListPage";
-import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "Gooburger - Hamburgers Deliciosos",
-  keywords: ["hamburgers", "entregas", "gooburger", "hamburgers deliciosos", "Peça um online"],
-  authors: [{ name: "Gooburger Team", url: "https://goo-burguer.vercel.app/" }],
-  creator: "Equipa Gooburger",
-  publisher: "Gooburger",
-  themeColor: "#ff5722",
-  colorScheme: "light dark",
-  description: "Explore nosso menu de hambúrgueres deliciosos no Gooburger. Encontre seu hambúrguer favorito e peça online para entrega ou retirada.",
-  openGraph: {
-    title: "Gooburger - Hamburgers Deliciosos",
-    description: "Explore nosso menu de hambúrgueres deliciosos no Gooburger. Encontre seu hambúrguer favorito e peça online para entrega ou retirada.",
-    images: [
-      {
-        url: "/images/7.jpg",
-        width: 1200,
-        height: 630,
-        alt: "Gooburger - Delicious Hamburgers Deliciosos",
-      },
-    ],
-  },
-  twitter: {
-    title: "Gooburger - Hamburgers Deliciosos",
-    description: "Explore nosso menu de hambúrgueres deliciosos no Gooburger. Encontre seu hambúrguer favorito e peça online para entrega ou retirada.",
-    card: "summary_large_image",
-    images: ["/images/7.jpg"],
-  }, 
-  robots: {
-    index: true,
-    follow: true,
-  },
-};
 export default function Home() {
   const [burgers, setBurgers] = useState([]);
 
